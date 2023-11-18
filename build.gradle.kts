81cd2f9598cd6cddf0696af7f80746f5fd67e6d7
DenimRed <76877830+DenimRed@users.noreply.github.com>
2023-11-18 15:52:39 -0700

    Refine build script
    Add maven publication and additional build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("org.spongepowered.gradle.vanilla") version "0.2.1-SNAPSHOT"
+    id("maven-publish")
 }
 
 group = "dev.denimred"
@@ -10,3 +11,23 @@ base.archivesName = "littlethings"
 minecraft { version("1.18.2") }
 
 tasks.jar { from("LICENSE") }
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release = 17
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = base.archivesName.get()
+            from(components["java"])
+        }
+    }
+    repositories { mavenLocal() }
+}
