7d6f681508e9346130d43494a5a0ad4317fe0e76
DenimRed <76877830+DenimRed@users.noreply.github.com>
2024-02-18 13:36:41 -0700

    Make TODO generic type less annoying looking

--- things/todo/src/main/java/dev/denimred/littlethings/todo/Todo.java
+++ things/todo/src/main/java/dev/denimred/littlethings/todo/Todo.java
@@ -18,7 +18,7 @@ public final class Todo extends Error {
      * Immediately throws an exception, but done in a way that satisfies type requirements.
      * Therefore, you can assign a variable to this function or return the result of this function and still have code that will compile.
      *
-     * @param <NOTHING> the "returned" type; technically discarded as this function never returns.
+     * @param <$> the "returned" type; technically discarded as this function never returns.
      *
      * @return nothing at all, as this function always throws an exception.
      *
@@ -27,7 +27,7 @@ public final class Todo extends Error {
     @SuppressWarnings("DeprecatedIsStillUsed")
     @Deprecated(forRemoval = true) // To emit warning/error
     @Contract(value = "-> fail", pure = true)
-    public static <NOTHING> NOTHING TODO() throws Todo {
+    public static <$> $ TODO() throws Todo {
         throw new Todo().trimStackTrace();
     }
 
@@ -36,7 +36,7 @@ public final class Todo extends Error {
      * Therefore, you can assign a variable to this function or return the result of this function and still have code that will compile.
      *
      * @param msg the message to be displayed that explains what is incomplete.
-     * @param <NOTHING> the "returned" type; technically discarded as this function never returns.
+     * @param <$> the "returned" type; technically discarded as this function never returns.
      *
      * @return nothing at all, as this function always throws an exception.
      *
@@ -45,7 +45,7 @@ public final class Todo extends Error {
     @SuppressWarnings("DeprecatedIsStillUsed")
     @Deprecated(forRemoval = true) // To emit warning/error
     @Contract(value = "_ -> fail", pure = true)
-    public static <NOTHING> NOTHING TODO(String msg) throws Todo {
+    public static <$> $ TODO(String msg) throws Todo {
         throw new Todo(msg).trimStackTrace();
     }
 
